409292d39181412c4c39043f1da0481537c50450
Tulun <jaykiraly@gmail.com>
2016-02-04 19:00:11 -0800

    Added controller to workout routeProvider.

--- js/app.js
+++ js/app.js
@@ -3,18 +3,10 @@
 
 angular.module('app', ['ngRoute', '7minWorkout']).
   config(function ($routeProvider) {
-    $routeProvider.when('/start', {
-      templateUrl: 'partials/start.html'
-    });
-    $routeProvider.when('/workout', {
-      templateUrl: 'partials/workout.html'
-    });
-    $routeProvider.when('/finish', {
-      templateUrl: 'partials/finish.html'
-    });
-    $routeProvider.otherwise({
-      redirectTo: '/start'
-    });
+    $routeProvider.when('/start', { templateUrl: 'partials/start.html'});
+    $routeProvider.when('/workout', { templateUrl: 'partials/workout.html', controller: 'WorkoutController'});
+    $routeProvider.when('/finish', { templateUrl: 'partials/finish.html'});
+    $routeProvider.otherwise({redirectTo: '/start'});
   });
 
 angular.module('7minWorkout', []);
